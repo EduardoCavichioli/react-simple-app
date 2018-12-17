bdc2cc31ec042e8aae06abc35bd87e78e909ea7a
EduardoCavichioli <edcavichioli@gmail.com>
2018-12-17 16:25:16 -0200

    filter added

--- list-items/src/components/App.js
+++ list-items/src/components/App.js
@@ -1,47 +1,70 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { Button } from 'reactstrap';
-
 class App extends Component {
   state = {
     listOfItems: ["banana","maca"],
-    inputValue: ''
+    inputValue: '',
+    filterValue: ''
   }
 
-  onInputChange = (event) => {
+  handleInputChange = (event) => {
     this.setState({
       inputValue: event.target.value
     })
   }
 
-  handleInsertItem = (value) => {
-    let list = this.state.listOfItems;
-    list.push(value);
-
+  handleFilterChange = (event) => {
     this.setState({
-      listOfItems: list
+      filterValue: event.target.value
     })
   }
 
-  render() {
+  handleSubmit = (event) => {
     let { listOfItems, inputValue } = this.state;
+    listOfItems.push(inputValue);
+
+    this.setState({
+      listOfItems: listOfItems
+    })
+    event.preventDefault();
+  }
+
+  checkFilter = (value) => {
+    let { filterValue } = this.state;
+
+    if (filterValue === '') {
+      return true;
+    } else if (value.includes(filterValue)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  render() {
+    let { listOfItems, inputValue, filterValue } = this.state;
+
+    let filteredList = listOfItems.filter(this.checkFilter);
     return (
       <div className="App">
         <div className="title">
           My React App
         </div>
-        <div>
-          <input 
-            onChange={this.onInputChange}
-            type="text"
-            value={inputValue}
-            />
-          <button className="btn btn-primary" onClick={() => this.handleInsertItem(inputValue)}>Add</button>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="my-input-value">Value</label>
+            <input type="text" className="form-control" id="my-input-value" value={inputValue} onChange={this.handleInputChange} />
+          </div>
+          <button className="btn btn-primary" type="submit">Submit</button>
+        </form>
+        <div className="form-group">
+          <label htmlFor="my-filter-value">Filter</label>
+          <input type="text" className="form-control" id="my-filter-value" value={filterValue} onChange={this.handleFilterChange}/>
         </div>
         <div>
           <ul>
-            {listOfItems.map((item, index) => (
+            {filteredList.map((item, index) => (
               <li key={index}>{item}</li>
             ))}
           </ul>
