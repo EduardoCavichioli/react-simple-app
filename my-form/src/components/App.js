f9fa45bc946cd7a9c37cc5c4020d227a985b817e
EduardoCavichioli <edcavichioli@gmail.com>
2018-12-18 11:50:26 -0200

    form component updates and callback

--- my-form/src/components/App.js
+++ my-form/src/components/App.js
@@ -3,11 +3,25 @@ import React, { Component } from 'react';
 import Form from './Form/Form';
 
 class App extends Component {
+  state = {
+    formData: {
+      name: '',
+      city: ''
+    }
+  }
+
+  formCallback = (formDataFromChild) => {
+    this.setState({
+      formData: formDataFromChild
+    })
+  }
+
   render() {
+    let { formData } = this.state;
     return (
       <div>
         <h2>My React App</h2>
-        <Form />
+        <Form callbackFromParent={this.formCallback}/>
       </div>
     );
   }
--- my-form/src/components/Form/Form.js
+++ my-form/src/components/Form/Form.js
@@ -1,16 +1,46 @@
 import React,{ Component } from 'react';
 
 class Form extends Component {
+  state = {
+    name: '',
+    city: ''
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const id = target.id;
+
+    this.setState({
+      [id]: value
+    })
+  }
+
+  handleSubmit = (event) => {
+    this.props.callbackFromParent(this.state);
+    this.setState({
+      name: '',
+      city: ''
+    })
+    event.preventDefault();
+  }
+
   render() {
+    let { name, city } = this.state;
     return(
       <div className="form-wrapper">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-row">
-            <div className="form-group col-md-5">
+            <div className="form-group col-md-6">
               <label htmlFor="name">Name</label>
-              <input type="text" className="form-control" id="name" placeholder="Name" />
+              <input type="text" className="form-control" id="name" placeholder="Name" value={name} onChange={this.handleInputChange} />
+            </div>
+            <div className="form-group col-md-6">
+              <label htmlFor="city">City</label>
+              <input type="text" className="form-control" id="city" placeholder="City" value={city} onChange={this.handleInputChange} />
             </div>
           </div>
+          <button type="submit" className="btn btn-primary">Send</button>
         </form>
       </div>
     )
