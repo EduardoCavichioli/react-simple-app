4f99a5ca18f1ab07dfa66728c2b1d900c7a3229e
Eduardo Cavichioli <eduardocavichioli@Eduardos-MacBook-Pro.local>
2018-12-17 23:43:53 -0200

    added form to save with enter key

--- list-items/src/components/InputForm/InputForm.js
+++ list-items/src/components/InputForm/InputForm.js
@@ -23,20 +23,22 @@ class InputForm extends Component {
     let { value } = this.state;
     return (
       <div>
-        <div className="input-group mb-3">
-          <input 
-            type="text" 
-            className="form-control"
-            onChange={this.handleInputChange}
-            value={value}
-            placeholder="Value"
-            aria-label="value"
-            aria-describedby="value-input"
-            />
-          <div className="input-group-append">
-            <button className="btn btn-outline-primary" onClick={this.handleSubmit}>Add</button>
+        <form onSubmit={this.handleSubmit}>
+          <div className="input-group mb-3">
+            <input 
+              type="text" 
+              className="form-control"
+              onChange={this.handleInputChange}
+              value={value}
+              placeholder="Value"
+              aria-label="value"
+              aria-describedby="value-input"
+              />
+            <div className="input-group-append">
+              <button className="btn btn-outline-primary" type="submit">Submit</button>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     )
   }
